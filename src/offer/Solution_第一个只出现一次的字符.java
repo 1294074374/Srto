b22701a89398dae165057dcc988ca7b8d7f36ed2
lenovo <lenovo@lenovo-PC>
2019-07-28 00:21:27 +0800

    2019-7-28

--- src/offer/Solution_第一个只出现一次的字符.java
+++ src/offer/Solution_第一个只出现一次的字符.java
@@ -1,10 +1,13 @@
 package offer;
 
+import java.util.HashMap;
+
 public class Solution_第一个只出现一次的字符 {
 	public int FirstNotRepeatingChar(String str) {
 		if (str == null) {
 			return -1;
 		}
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		return 0;
 	}
 }
